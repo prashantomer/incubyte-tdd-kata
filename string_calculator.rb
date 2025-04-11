746c24c4a5b5a8b3d4e330d77a2241667ff248e8
Prashant Omer <omer.prashant@gmail.com>
2025-04-11 15:02:41 +0530

    Step 1 - Handle Single number in string, ex: '5'

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,8 @@
 class StringCalculator
   class << self
-    def add(numbers)
-      return 0 if numbers.empty?
+    def add(numbers_string)
+      return 0 if numbers_string.empty?
+      return numbers_string.to_i if numbers_string =~ /^\d+$/
     end
   end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe StringCalculator do
   it "returns 0 for an empty string" do
     expect(StringCalculator.add("")).to eq(0)
   end
+
+  # Test for a single number
+  it "returns the number itself for a single number" do
+    expect(StringCalculator.add("5")).to eq(5)
+  end
 end
