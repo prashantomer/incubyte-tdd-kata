89af9221c8994c4f272411aa03fd8dca623b58ef
Prashant Omer <omer.prashant@gmail.com>
2025-04-11 17:06:28 +0530

    Refactor

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,27 @@ class StringCalculator
   class << self
     def add(numbers_string)
       return 0 if numbers_string.empty?
-      numbers_string.split(/[,;\n]/).map(&:to_i).sum
+      new(numbers_string).add
     end
+  end
+
+  attr :numbers_string
+
+  def initialize(numbers_string)
+    @numbers_string = numbers_string
+  end
+
+  def add
+    return 0 if numbers_string.empty?
+    numbers.inject(0) { |sum, number| number.to_i + sum }
+  end
+
+  private
+  def numbers
+    numbers_string.split(default_delimiter)
+  end
+
+  def default_delimiter
+    /[,;\n]/
   end
 end
